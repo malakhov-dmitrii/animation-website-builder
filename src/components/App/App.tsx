172a885f2e1f839b1983290efc1c055bfd91e1fe
BJuice1984 <beetle_juice84@mail.ru>
2024-02-19 21:56:42 +0300

    feat, style(App.tsx, index.scss): create new components with scss

---
+++ src/components/App/App.tsx
@@ -0,0 +1,13 @@
+import Layout from '../Layout/Layout'
+
+function App() {
+    return (
+        <div className='page'>
+            <div className='page__container'>
+                <Layout />
+            </div>
+        </div>
+    )
+}
+
+export default App
---
+++ src/components/Layout/Layout.tsx
@@ -0,0 +1,9 @@
+function Layout() {
+    return (
+        <section className='layout'>
+            <h1 className='layout__title'>Animation Settings</h1>
+        </section>
+    )
+}
+
+export default Layout
