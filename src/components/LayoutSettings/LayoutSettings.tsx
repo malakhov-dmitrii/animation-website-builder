786a91835c6fae3dde41a78e58779bf5f51c4fde
BJuice1984 <beetle_juice84@mail.ru>
2024-02-21 22:14:31 +0300

    feat(LayoutSettings.tsx, Slider.tsx, slider.scss): create all sliders + fix styles

--- src/components/LayoutSettings/LayoutSettings.tsx
+++ src/components/LayoutSettings/LayoutSettings.tsx
@@ -3,13 +3,13 @@ import Slider from '../Slider/Slider'
 function LayoutSettings() {
     return (
         <section className='layout-settings'>
-            <Slider sliderName={'X'} />
-            <Slider sliderName={'Y'} />
-            <Slider sliderName={'Opacity'} />
-            <Slider sliderName={'Scale'} />
-            <Slider sliderName={'Blur'} />
-            <Slider sliderName={'Speed'} />
-            <Slider sliderName={'Delay'} />
+            <Slider sliderName={'X'} fromCenter={true} />
+            <Slider sliderName={'Y'} fromCenter={true} />
+            <Slider sliderName={'Opacity'} fromCenter={false} />
+            <Slider sliderName={'Scale'} fromCenter={true} />
+            <Slider sliderName={'Blur'} fromCenter={false} />
+            <Slider sliderName={'Speed'} fromCenter={false} />
+            <Slider sliderName={'Delay'} fromCenter={false} />
         </section>
     )
 }
--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -2,13 +2,47 @@ import { useState } from 'react'
 
 type TSliderProps = {
     sliderName: string
+    fromCenter: boolean
 }
 
-function Slider({ sliderName }: TSliderProps) {
-    const [value, setValue] = useState(0)
+function Slider({ sliderName, fromCenter }: TSliderProps) {
+    const minValue = -100
+    const maxValue = 100
+    const [value, setValue] = useState(fromCenter ? 0 : minValue)
 
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         setValue(parseInt(e.target.value))
+    }
+
+    const roundValue = (
+        value: number,
+        minValue: number,
+        maxValue: number,
+        outputMin: number,
+        outputMax: number
+    ) => {
+        return ((value - minValue) * (outputMax - outputMin)) / (maxValue - minValue) + outputMin
+    }
+
+    const renderValue = (value: number, sliderName: string) => {
+        switch (sliderName) {
+            case 'Opacity':
+                return `${roundValue(value, minValue, maxValue, 0, 100).toFixed(0.1)}%`
+            case 'Scale':
+                return roundValue(value, minValue, maxValue, 0.1, 2.0).toFixed(1)
+            case 'Blur':
+                return roundValue(value, minValue, maxValue, 0, 10).toFixed(0.1)
+            case 'Speed':
+                return `${roundValue(value, minValue, maxValue, 0, 5)
+                    .toFixed(1)
+                    .replace(/^0+/, '')}s`
+            case 'Delay':
+                return `${roundValue(value, minValue, maxValue, 0, 10)
+                    .toFixed(1)
+                    .replace(/^0+/, '')}s`
+            default:
+                return value
+        }
     }
 
     return (
@@ -18,20 +52,31 @@ function Slider({ sliderName }: TSliderProps) {
                 <input
                     className='slider__input'
                     type='range'
-                    min={-100}
-                    max={100}
+                    min={minValue}
+                    max={maxValue}
                     value={value}
                     onChange={handleChange}
                 />
                 <div className='slider__track'></div>
-                <div
-                    className='slider__track-progress'
-                    style={{
-                        width: `${value > 0 ? value / 2.5 : (value / 2.5) * -1}%`,
-                        transformOrigin: `${value < 0 ? 'left center' : 'none'}`,
-                        transform: `${value < 0 ? 'rotate(180deg)' : 'none'}`,
-                    }}
-                />
+                {fromCenter ? (
+                    <div
+                        className='slider__track-progress'
+                        style={{
+                            width: `${value > 0 ? value / 2.5 : (value / 2.5) * -1}%`,
+                            transformOrigin: `${value < 0 ? 'left center' : 'none'}`,
+                            transform: `${value < 0 ? 'rotate(180deg)' : 'none'}`,
+                        }}
+                    />
+                ) : (
+                    <div
+                        className='slider__track-progress slider__track-progress_type_left'
+                        style={{
+                            width: `${
+                                (Math.max(0, value - minValue) / (maxValue - minValue)) * 100
+                            }%`,
+                        }}
+                    />
+                )}
                 <div
                     className='slider__thumb'
                     style={{
@@ -39,7 +84,7 @@ function Slider({ sliderName }: TSliderProps) {
                     }}
                 />
             </div>
-            <output className='slider__output'>{value}</output>
+            <output className='slider__output'>{renderValue(value, sliderName)}</output>
         </div>
     )
 }
